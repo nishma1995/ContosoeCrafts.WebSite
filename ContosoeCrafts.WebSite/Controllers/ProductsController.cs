ccaa038e6efe608d5a64d11fc4ae23b134001c11
nishma1995 <nishmakk@gmail.com>
2021-07-13 17:15:34 -0700

    next

---
+++ ContosoeCrafts.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using ContosoeCrafts.WebSite.Models;
+using ContosoeCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoeCrafts.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductsService productsService)
+        {
+            this.ProductsService = productsService;
+        }
+        public JsonFileProductsService ProductsService { get; }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+
+        {
+            return ProductsService.GetProducts();
+        }
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery]string ProductId,[FromQuery]int Rating)
+        {
+            ProductsService.AddRating(ProductId, Rating);
+                return Ok();
+        }
+    }
+}
